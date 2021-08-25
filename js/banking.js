aa84eeb129e02bca2f254ced7d51a57e15eb4e41
zihad-c <zihad.cit.bd@gmail.com>
2021-08-25 06:36:47 +0600

    error validation added

--- js/banking.js
+++ js/banking.js
@@ -1,83 +1,111 @@
 // ============= update deposit ==============
 
+// getting deposit button
 const depositBtn = document.getElementById('deposit-btn');
+
+// getting deposit input field
+const depositInput = document.getElementById('deposit-input');
+
+// getting error message id
+const errorMessage = document.getElementById('error-message');
 
 // adding an event listener 
 depositBtn.addEventListener('click', function () {
-    // getting deposit input field
-    const depositInput = document.getElementById('deposit-input');
-    const depositInputValueInText = depositInput.value;
-    const depositInputValueInNumber = parseFloat(depositInputValueInText);
+    if (depositInput.value > 0) {
 
-    // getting deposit total text 
-    const depositTotal = document.getElementById('deposit-total');
-    const depositTotalInText = depositTotal.innerText;
-    const depositTotalInNumber = parseFloat(depositTotalInText);
-    depositTotal.innerText = depositInputValueInNumber;
-    const newDepositTotal = depositInputValueInNumber + depositTotalInNumber;
+        const depositInputValueInText = depositInput.value;
+        const depositInputValueInNumber = parseFloat(depositInputValueInText);
 
-    // clearing the deposit input field
-    depositInput.value = "";
+        // getting deposit total text 
+        const depositTotal = document.getElementById('deposit-total');
+        const depositTotalInText = depositTotal.innerText;
+        const depositTotalInNumber = parseFloat(depositTotalInText);
+        depositTotal.innerText = depositInputValueInNumber;
+        const newDepositTotal = depositInputValueInNumber + depositTotalInNumber;
+        // clearing the deposit input field
+        depositInput.value = "";
 
-    // update deposit total amount
-    depositTotal.innerText = newDepositTotal;
+        // update deposit total amount
+        depositTotal.innerText = newDepositTotal;
 
+        // getting balance total inner-text
+        const balanceTotal = document.getElementById('balance-total');
+        const balanceTotalInText = balanceTotal.innerText;
 
-    // getting balance total inner-text
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalInText = balanceTotal.innerText;
+        // convert balance total in number 
+        const balanceTotalInNumber = parseFloat(balanceTotalInText);
 
-    // convert balance total in number 
-    const balanceTotalInNumber = parseFloat(balanceTotalInText);
+        // update new balance total
+        const newBalanceTotal = depositInputValueInNumber + balanceTotalInNumber;
 
-    // update new balance total
-    const newBalanceTotal = depositInputValueInNumber + balanceTotalInNumber;
+        // update balance total inner-text 
+        balanceTotal.innerText = newBalanceTotal;
 
-    // update balance total inner-text 
-    balanceTotal.innerText = newBalanceTotal;
+        // error message hidden 
+        errorMessage.style.display = "none";
+    }
+
+    else {
+        // error message shown 
+        errorMessage.style.display = "block";
+    }
 })
 
 
 // ================ update withdraw =============
 
+// getting withdraw button 
 const withdrawBtn = document.getElementById('withdraw-btn');
+
+// getting withdraw input field 
+const withdrawInput = document.getElementById('withdraw-input');
 
 // adding another event listener 
 withdrawBtn.addEventListener('click', function () {
-    // getting withdraw input field 
-    const withdrawInput = document.getElementById('withdraw-input');
-    // getting withdraw input value 
-    const withdrawInputValueInText = withdrawInput.value;
+    if (withdrawInput.value > 0) {
+        // getting withdraw input field 
+        const withdrawInput = document.getElementById('withdraw-input');
+        // getting withdraw input value 
+        const withdrawInputValueInText = withdrawInput.value;
 
-    // convert withdraw input value in number 
-    const withdrawInputValueInNumber = parseFloat(withdrawInputValueInText)
+        // convert withdraw input value in number 
+        const withdrawInputValueInNumber = parseFloat(withdrawInputValueInText)
 
 
-    // getting withdraw total text 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const withdrawTotalInText = withdrawTotal.innerText;
-    withdrawTotal.innerText = withdrawInputValueInNumber;
-    const withdrawTotalInNumber = parseFloat(withdrawTotalInText);
-    // getting new withdraw amount 
-    const newWithdrawTotal = withdrawTotalInNumber + withdrawInputValueInNumber;
+        // getting withdraw total text 
+        const withdrawTotal = document.getElementById('withdraw-total');
+        const withdrawTotalInText = withdrawTotal.innerText;
+        withdrawTotal.innerText = withdrawInputValueInNumber;
+        const withdrawTotalInNumber = parseFloat(withdrawTotalInText);
+        // getting new withdraw amount 
+        const newWithdrawTotal = withdrawTotalInNumber + withdrawInputValueInNumber;
 
-    // update total withdraw amount 
-    withdrawTotal.innerText = newWithdrawTotal;
+        // update total withdraw amount 
+        withdrawTotal.innerText = newWithdrawTotal;
 
-    // clearing withdraw input field 
-    withdrawInput.value = "";
+        // clearing withdraw input field 
+        withdrawInput.value = "";
 
-    // getting balance total inner-text
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalInText = balanceTotal.innerText;
+        // getting balance total inner-text
+        const balanceTotal = document.getElementById('balance-total');
+        const balanceTotalInText = balanceTotal.innerText;
 
-    // convert balance total in number 
-    const balanceTotalInNumber = parseFloat(balanceTotalInText);
+        // convert balance total in number 
+        const balanceTotalInNumber = parseFloat(balanceTotalInText);
 
-    // update new balance total
-    const newBalanceTotal = balanceTotalInNumber - withdrawInputValueInNumber;
+        // update new balance total
+        const newBalanceTotal = balanceTotalInNumber - withdrawInputValueInNumber;
 
-    // update balance total inner-text 
-    balanceTotal.innerText = newBalanceTotal;
+        // update balance total inner-text 
+        balanceTotal.innerText = newBalanceTotal;
+
+        // error message hidden 
+        errorMessage.style.display = "none";
+    }
+
+    else {
+        // error message shown 
+        errorMessage.style.display = "block";
+    }
 
 })
